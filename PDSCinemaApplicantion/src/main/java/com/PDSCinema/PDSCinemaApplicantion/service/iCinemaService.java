17bd65f0aa12cf6123907c2d36968daa80e87354
Acsa Arcanjo <44912056+acsa-laa@users.noreply.github.com>
2021-02-17 22:25:38 -0300

    Update iCinemaService.java

--- PDSCinemaApplicantion/src/main/java/com/PDSCinema/PDSCinemaApplicantion/service/iCinemaService.java
+++ PDSCinemaApplicantion/src/main/java/com/PDSCinema/PDSCinemaApplicantion/service/iCinemaService.java
@@ -1,13 +1,13 @@
 package com.PDSCinema.PDSCinemaApplicantion.service;
 
 public interface iCinemaService {
-   public void inserirCliente(Cliente cliente);
-    public void removerCliente(Cliente cliente);
-    public void buscarCliente(Cliente cliente);
-    public List <Cliente> buscarTodosCliente();
-    public void inserirADM(Administrador ADM);
-    public void removerADM(Administrador ADM);
-    public void buscarADM(Administrador ADM);
-    public List <Administrador> buscarTodosADM();
-    public void calcularMediaAvaliacao();
+    void inserirCliente(Cliente cliente);
+    void removerCliente(Cliente cliente);
+    void buscarCliente(Cliente cliente);
+    List <Cliente> buscarTodosCliente();
+    void inserirADM(Administrador ADM);
+    void removerADM(Administrador ADM);
+    void buscarADM(Administrador ADM);
+    List <Administrador> buscarTodosADM();
+    void calcularMediaAvaliacao();
 }
