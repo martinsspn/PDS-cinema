64a183a2dcb3d19aad96f6e160235591eeb12b86
Acsa Arcanjo <44912056+acsa-laa@users.noreply.github.com>
2021-02-17 22:04:00 -0300

    Update CinemaService.java

--- PDSCinemaApplicantion/src/main/java/com/PDSCinema/PDSCinemaApplicantion/service/CinemaService.java
+++ PDSCinemaApplicantion/src/main/java/com/PDSCinema/PDSCinemaApplicantion/service/CinemaService.java
@@ -6,15 +6,39 @@ public class CinemaService implements iCinemaService{
     private Cinema cinema;
 
     @Override
-    public void avaliarServico(int avaliacao){
+    public void inserirCliente(Cliente cliente){
+
+    }
+     @Override
+    public void removerCliente(Cliente cliente){
+
+    }
+     @Override
+    public void buscarCliente(Cliente cliente){
+
+    } 
+    @Override
+    public <Cliente> buscarTodosCliente(){
+
+    }
+     @Override
+    public void inserirADM(Administrador ADM){
 
     }
     @Override
-    public void avaliarHorario(int avaliacao){
+    public void removerADM(Administrador ADM){
 
     }
+     @Override
+    public void buscarADM(Administrador ADM){
+
+    } 
     @Override
-    public void avaliarFilme(int avaliacao){
+    public <Administrador> buscarTodosADM(){
+
+    }
+     @Override
+    public void calcularMediaAvaliacao(){
 
     }
 }
