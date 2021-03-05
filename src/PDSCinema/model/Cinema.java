36654faf7f245a265bb102a9ba3a5588856004d8
Acsa Arcanjo <44912056+acsa-laa@users.noreply.github.com>
2021-03-05 14:56:06 -0300

    Add files via upload

---
+++ src/PDSCinema/model/Cinema.java
@@ -0,0 +1,119 @@
+package PDSCinema.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cinema {
+    private List<Integer> salas;
+    private List<String> listaClientesCpf;
+    private List<Cliente> listaClientes;
+    private List<String> listaAdministradorCpf;
+    private List<Administrador> listaAdministrador;
+    private List<Filme> filmesEmCartaz;
+    private List<String> horarios;
+    private int avaliacoesServico;
+    private List<Integer> avaliacoesHorarios;
+    private int quantAvServico;
+    private List<Integer> quantAvHorarios;
+
+    public Cinema() {
+        this.salas = new ArrayList<>();
+        this.listaClientesCpf = new ArrayList<>();
+        this.listaClientes = new ArrayList<>();
+        this.listaAdministradorCpf = new ArrayList<>();
+        this.listaAdministrador = new ArrayList<>();
+        this.filmesEmCartaz = new ArrayList<>();
+        this.horarios = new ArrayList<>();
+        this.avaliacoesServico = 0;
+        this.avaliacoesHorarios = new ArrayList<>();
+        this.quantAvServico = 0;
+        this.quantAvHorarios = new ArrayList<>();
+    }
+
+    public List<Integer> getSalas() {
+        return salas;
+    }
+
+    public void setSalas(List<Integer> salas) {
+        this.salas = salas;
+    }
+
+    public List<String> getListaAdministradorCpf() {
+        return listaAdministradorCpf;
+    }
+
+    public void setListaAdministradorCpf(List<String> ListaAdministradorCpf) {
+        this.listaAdministradorCpf = listaAdministradorCpf;
+    }
+
+    public List<String> getListaClientesCpf() {
+        return listaClientesCpf;
+    }
+
+    public void setListaClientesCpf(List<String> listaClientesCpf) {
+        this.listaClientesCpf = listaClientesCpf;
+    }
+
+    public List<Filme> getFilmesEmCartaz() {
+        return filmesEmCartaz;
+    }
+
+    public void setFilmesEmCartaz(List<Filme> filmesEmCartaz) {
+        this.filmesEmCartaz = filmesEmCartaz;
+    }
+
+    public int getAvaliacoesServico() {
+        return avaliacoesServico;
+    }
+
+    public void setAvaliacoesServico(int avaliacoesServico) {
+        this.avaliacoesServico = avaliacoesServico;
+    }
+
+    public int getQuantAvServico() {
+        return quantAvServico;
+    }
+
+    public void setQuantAvServico(int quantAvServico) {
+        this.quantAvServico = quantAvServico;
+    }
+
+    public List<Integer> getAvaliacoesHorarios() {
+        return avaliacoesHorarios;
+    }
+
+    public void setAvaliacoesHorarios(List<Integer> avaliacoesHorarios) {
+        this.avaliacoesHorarios = avaliacoesHorarios;
+    }
+    public List<Cliente> getListaClientes() {
+        return listaClientes;
+    }
+
+    public void setListaClientes(List<Cliente> listaClientes) {
+        this.listaClientes = listaClientes;
+    }
+
+    public List<Administrador> getListaAdministrador() {
+        return listaAdministrador;
+    }
+
+    public void setListaAdministrador(List<Administrador> listaAdministrador) {
+        this.listaAdministrador = listaAdministrador;
+    }
+    public List<String> getHorarios() {
+        return horarios;
+    }
+
+    public void setHorarios(List<String> horarios) {
+        this.horarios = horarios;
+    }
+
+    public List<Integer> getQuantAvHorarios() {
+        return quantAvHorarios;
+    }
+
+    public void setQuantAvHorarios(List<Integer> quantAvHorarios) {
+        this.quantAvHorarios = quantAvHorarios;
+    }
+}
+
---
+++ src/PDSCinema/model/Cliente.java
@@ -0,0 +1,34 @@
+package PDSCinema.model;
+
+import java.util.List;
+
+
+public class Cliente extends Usuario {
+    private List<Ingresso> ingressos;
+    private List<Cupom> cupons;
+    private List<Premio> premios;
+
+    public List<Ingresso> getIngressos() {
+        return ingressos;
+    }
+
+    public void setIngressos(List<Ingresso> ingressos) {
+        this.ingressos = ingressos;
+    }
+
+    public List<Cupom> getCupons() {
+        return cupons;
+    }
+
+    public void setCupons(List<Cupom> cupons) {
+        this.cupons = cupons;
+    }
+
+    public List<Premio> getPremios() {
+        return premios;
+    }
+
+    public void setPremios(List<Premio> premios) {
+        this.premios = premios;
+    }
+}
