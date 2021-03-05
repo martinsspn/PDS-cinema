a1ad6737d261fc9d312cf646162d8a889b160b6c
Gabriel Lucas <gabrielucasml@gmail.com>
2021-03-05 13:33:58 -0300

    Implementação das funções de Ranking

--- PDSCinemaApplicantion/src/main/java/com/PDSCinema/PDSCinemaApplicantion/service/CinemaService.java
+++ PDSCinemaApplicantion/src/main/java/com/PDSCinema/PDSCinemaApplicantion/service/CinemaService.java
@@ -119,4 +119,61 @@ public class CinemaService implements iCinemaService{
     	}
     	return valor;
     }
+
+	public ArrayList<String> calcularRankingHorarios(List<String> horarios, List<Integer> avaliacoesHorarios, List<Integer> quantAvHorarios){
+		List<Double> medias = calcularMediaAvaliacaoHorario(avaliacoesHorarios, quantAvHorarios);
+		ArrayList<String> rankingHorarios = new ArrayList<>();
+
+		for(String horario : horarios)
+			rankingHorarios.add(horario);
+
+		int i, j, min;
+		Double aux;
+		String aux2;
+		for (i = 0; i < (horarios.size())-1; i++){
+			min = i;
+			for (j = (i + 1); j < horarios.size(); j++) {
+				if (medias.get(j) < medias.get(min))
+					min = j;
+			}
+			if (i != min) {
+				aux = medias.get(i);
+				aux2 = rankingHorarios.get(i);
+				medias.set(i, medias.get(min));
+				medias.set(min, aux);
+				rankingHorarios.set(i, rankingHorarios.get(min));
+				rankingHorarios.set(min, aux2);
+			}
+		}
+		return rankingHorarios;
+	}
+
+    public ArrayList<String> calcularRankingFilme(List<Filme> filmesEmCartaz){
+    	List<Double> medias = calcularMediaAvaliacaoFilmes(filmesEmCartaz);
+    	ArrayList<String> rankingFilmes = new ArrayList<>();
+
+    	for(Filme filme : filmesEmCartaz)
+    		rankingFilmes.add(filme.getName());
+
+		int i, j, min;
+		Double aux;
+		String aux2;
+		for (i = 0; i < (filmesEmCartaz.size())-1; i++){
+			min = i;
+			for (j = (i + 1); j < filmesEmCartaz.size(); j++) {
+				if (medias.get(j) < medias.get(min))
+					min = j;
+			}
+			if (i != min) {
+				aux = medias.get(i);
+				aux2 = rankingFilmes.get(i);
+				medias.set(i, medias.get(min));
+				medias.set(min, aux);
+				rankingFilmes.set(i, rankingFilmes.get(min));
+				rankingFilmes.set(min, aux2);
+			}
+		}
+		return rankingFilmes;
+	}
+
 }
--- PDSCinemaApplicantion/src/main/java/com/PDSCinema/PDSCinemaApplicantion/service/iClienteService.java
+++ PDSCinemaApplicantion/src/main/java/com/PDSCinema/PDSCinemaApplicantion/service/iClienteService.java
@@ -4,6 +4,9 @@ import com.PDSCinema.PDSCinemaApplicantion.model.Cinema;
 import com.PDSCinema.PDSCinemaApplicantion.model.Filme;
 import com.PDSCinema.PDSCinemaApplicantion.model.Ingresso;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface iClienteService {
     int avaliarServico(Cinema cinema, int avaliacao);
     int avaliarHorario(Cinema cinema, String horario ,int avaliacao);
