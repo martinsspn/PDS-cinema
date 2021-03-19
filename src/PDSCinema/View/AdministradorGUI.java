8d14b70aabe2cda79d01017511f035c4d2d101c8
Acsa Arcanjo <44912056+acsa-laa@users.noreply.github.com>
2021-03-19 19:48:19 -0300

    Update AdministradorGUI.java

--- src/PDSCinema/View/AdministradorGUI.java
+++ src/PDSCinema/View/AdministradorGUI.java
@@ -1,5 +1,7 @@
 package PDSCinema.View;
 
 public class AdministradorGUI {
+// pode ser tudo no construtor tbm como a de cinema, além de fazer as funções do adm
 
+// precisa ter um meio de voltar pro cinema no final, por exemplo perguntar se ele quer sair
 }
