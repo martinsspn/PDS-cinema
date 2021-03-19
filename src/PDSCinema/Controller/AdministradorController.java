2d43897d3e88adc027862b1b213bcc92fe787cb0
Gabriel Lucas <gabrielucasml@gmail.com>
2021-03-19 14:19:10 -0300

    Implementação da interface gráfica

--- src/PDSCinema/Controller/AdministradorController.java
+++ src/PDSCinema/Controller/AdministradorController.java
@@ -13,42 +13,34 @@ public class AdministradorController {
     AdministradorController(AdministradorService adm){
         this.adm = adm;
     }
-    public int cadastrarFilmes(CinemaRepository cinema, String name, int duracao, String sinopse, String classificacaoIndicativa, String genero,
+    public String cadastrarFilmes(CinemaRepository cinema, String name, int duracao, String sinopse, String classificacaoIndicativa, String genero,
                         String diaDeEstreia){
         int status = adm.cadastrarFilmes(cinema, name, duracao, sinopse, classificacaoIndicativa, genero, diaDeEstreia);
         if(status == 0){
-            System.out.println("Filme cadastrado com sucesso");
-            return 0;
+            return ("Filme cadastrado com sucesso");
         }else if(status == -1){
-            System.out.println("A duração não pode ser menor que 0!");
-            return -1;
+            return ("A duração não pode ser menor que 0!");
         }else if(status == -2){
-            System.out.println("Algum valor passado está nulo!");
-            return -2;
+            return ("Algum valor passado está nulo!");
         }
-        return -3;
+        return "Filme não cadastrado!";
     }
-    public int cadastrarCupons(CinemaRepository cinema, String _Cupom, double tipoDeCupom){
+    public String cadastrarCupons(CinemaRepository cinema, String _Cupom, double tipoDeCupom){
         int status = adm.cadastrarCupons(cinema, _Cupom, tipoDeCupom);
         if(status == 0){
-            System.out.println("Cupom cadastrado com sucesso");
-            return 0;
+            return ("Cupom cadastrado com sucesso");
         }else{
-            System.out.println("Nome do cupom ou tipo do cupom inválido!");
-            return -1;
+            return ("Nome do cupom ou tipo do cupom inválido!");
         }
     }
-    public int cadastrarPremios(CinemaRepository cinema, String descricao, int id){
+    public String cadastrarPremios(CinemaRepository cinema, String descricao, int id){
         int status = adm.cadastrarPremios(cinema, descricao, id);
         if(status == 0){
-            System.out.println("Premio cadastrado com sucesso");
-            return 0;
+            return ("Premio cadastrado com sucesso");
         }else if(status == -1){
-            System.out.println("Este cupom já existe!");
-            return -1;
+            return ("Este cupom já existe!");
         }else{
-            System.out.println("Valor para o nome do cupom inválido!");
-            return -2;
+            return ("Valor para o nome do cupom inválido!");
         }
     }
 
@@ -89,34 +81,28 @@ public class AdministradorController {
         return null;
     }
 
-    public int removerFilmes(CinemaRepository cinema, Filme filme){
+    public String removerFilmes(CinemaRepository cinema, Filme filme){
         int status = adm.removerFilmes(cinema, filme);
         if(status == 0){
-            System.out.println("Filme removido com sucesso");
-            return 0;
+            return ("Filme removido com sucesso");
         }else{
-            System.out.println("Filme não cadastrado!");
-            return -1;
+            return ("Filme não cadastrado!");
         }
     }
-    public int removerCupons(CinemaRepository cinema, Cupom cupom){
+    public String removerCupons(CinemaRepository cinema, Cupom cupom){
         int status = adm.removerCupons(cinema, cupom);
         if(status == 0){
-            System.out.println("Cupom removido com sucesso");
-            return 0;
+            return ("Cupom removido com sucesso");
         }else{
-            System.out.println("Cupom não cadastrado!");
-            return -1;
+            return ("Cupom não cadastrado!");
         }
     }
-    public int removerPremios(CinemaRepository cinema, Premio premio){
+    public String removerPremios(CinemaRepository cinema, Premio premio){
         int status = adm.removerPremios(cinema, premio);
         if(status == 0){
-            System.out.println("Premio removido com sucesso");
-            return 0;
+            return ("Premio removido com sucesso");
         }else{
-            System.out.println("Premio não cadastrado!");
-            return -1;
+            return ("Premio não cadastrado!");
         }
     }
 }
